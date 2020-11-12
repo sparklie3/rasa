108b9f8dd0eaa2b78266bf3d668e40fc9a0309ad
shaw <shaw.s.li@gmail.com>
2020-11-11 20:51:46 -0500

    make further updates to get GPT2 working in cusotm actions

--- actions/actions.py
+++ actions/actions.py
@@ -1,13 +1,10 @@
-# from transformers import pipeline, AutoTokenizer, AutoModelWithLMHead
-
-# tokenizer = AutoTokenizer.from_pretrained("gpt2")
-
-# model = AutoModelWithLMHead.from_pretrained("gpt2")
+from transformers import pipeline, AutoTokenizer, AutoModelWithLMHead
 
 from typing import Any, Dict, List, Text, Union
-
+import time
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import BotUttered
 from rasa_sdk.events import SlotSet
 
 
@@ -20,11 +17,52 @@ class action_find_career_response(Action):
     def run(self,
         dispatcher: CollectingDispatcher,
         tracker: "Tracker",
-        domain: Dict[Text, Any]):
+        domain: Any):
 
-        bot = ("Reaching out to a human agent")
-        dispatcher.utter_message(bot)
+        # rasa_slot = tracker.get_slot('test_memory')
+        slot = SlotSet(
+            key = "test_memory",
+            value = "remember this"
+        )
 
-        dispatcher.utter_message("say hello")
-        return []
+        message = BotUttered(
+            text = "hello",
+            timestamp = time.time())
 
+        message2 = BotUttered(
+            text = "hello number 2",
+            timestamp = time.time())    
+        # dispatcher.utter_message(message) #return as response
+        # return []
+        return [slot] #return as event
+
+class generate_text(Action):
+    def name(self)-> Text:
+        return "action_generate_text"
+
+    def run(self,
+        dispatcher: CollectingDispatcher,
+        tracker: "Tracker",
+        domain: Any):
+        tokenizer = AutoTokenizer.from_pretrained("gpt2")
+        model = AutoModelWithLMHead.from_pretrained("gpt2")
+        text_generator = pipeline("text-generation", tokenizer=tokenizer, model=model)
+        seed_language = "I'm glad you're interested in this position. Let's chat about"
+        generated_text = text_generator(seed_language,max_length=100, do_sample=False)
+        
+        option1 = SlotSet(
+            key = "option1",
+            value = generated_text
+            # value = generated_test[0]
+        )
+        # option2 = SlotSet(
+        #     key = "option2"
+        #     value = generated_test[1]
+        # )
+        
+        # option3 = SlotSet(
+        #     key = "option3"
+        #     value = generated_test[2]
+        # )
+        dispatcher.utter_message("It worked. I've generated the following text.") #return as response
+        return [option1]
